ea9ed06a361f304d922121e61ee40ab3c26d8db0
vktrung <trung040102@gmail.com>
2024-10-17 09:35:43 +0700

    test

--- Thriftly/app/src/main/java/com/mastercoding/thriftly/MainActivity.java
+++ Thriftly/app/src/main/java/com/mastercoding/thriftly/MainActivity.java
@@ -11,7 +11,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.mastercoding.thriftly.Authen.SignIn;
 import com.mastercoding.thriftly.Authen.SignUpActivity;
 
 public class MainActivity extends AppCompatActivity {
