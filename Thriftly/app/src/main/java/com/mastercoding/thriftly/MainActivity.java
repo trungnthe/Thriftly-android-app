2a06db0d0e81d701165c0d17f8e97218aad8624b
vktrung <trung040102@gmail.com>
2024-10-18 13:47:53 +0700

    bottom_menu and login

--- Thriftly/app/src/main/java/com/mastercoding/thriftly/MainActivity.java
+++ Thriftly/app/src/main/java/com/mastercoding/thriftly/MainActivity.java
@@ -2,51 +2,108 @@ package com.mastercoding.thriftly;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.mastercoding.thriftly.Authen.SignInActivity;
+import com.mastercoding.thriftly.R;
+import com.mastercoding.thriftly.UI.HomeFragment;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
-import com.mastercoding.thriftly.Authen.SignUpActivity;
+public class MainActivity extends AppCompatActivity{
 
-public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private BottomNavigationView bottomNavigationView;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle toggle;
+    private HomeFragment homeFragment;
 
-    private Button btnSignIn;
-    private Button btnSignUp;
 
-    private void bindingView() {
-        btnSignIn = findViewById(R.id.btnSignIn);
-        btnSignUp = findViewById(R.id.btnSignUp);
+
+    private void bindingView(){
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+        homeFragment = new HomeFragment();
     }
 
-    private void bindingAction() {
-        btnSignIn.setOnClickListener(this::onSignIn);
-        btnSignUp.setOnClickListener(this::onSignUp);
-    }
+    private void bindingAction(){
 
-    private void onSignUp(View view) {
-        startActivity(new Intent(MainActivity.this, SignIn.class));
-    }
-
-    private void onSignIn(View view) {
-        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
         bindingView();
-        bindingAction();
+        setSupportActionBar(toolbar);
+
+
+
+        bottomNavigationView.setBackground(null);
+        setupBottomNavigation();
+
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() == null) {
+            Log.d("MainActivity", "No user is signed in");
+        }else{
+            Log.d("MainActivity", "User is signed in");
+        }
+
     }
+
+    private void setupBottomNavigation() {
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.menu_home) {
+                switchFragment(homeFragment);
+            } else if (id == R.id.menu_shorts) {
+                Log.d("MainActivity", "menu_shorts");
+                // replaceFragment(new ShortsFragment());
+            } else if (id == R.id.menu_subscriptions) {
+                Log.d("MainActivity", "menu_subscriptions");
+                // replaceFragment(new SubscriptionFragment());
+            } else if (id == R.id.menu_library) {
+                Log.d("MainActivity", "menu_library");
+                // replaceFragment(new LibraryFragment());
+            }
+            return true;
+        });
+    }
+
+
+
+    private void logout() {
+        // Firebase sign out
+        FirebaseAuth.getInstance().signOut();
+        // Navigate to the SignInActivity and clear back stack
+        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        // Finish the current activity to prevent going back to the main screen after logout
+        finish();
+    }
+
+
+    private void switchFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_layout, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
 }
