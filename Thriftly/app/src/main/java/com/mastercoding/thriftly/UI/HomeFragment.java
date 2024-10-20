99eb869b572c547c43bf38b8b438dbff50a49ad6
vktrung <trung040102@gmail.com>
2024-10-20 15:23:08 +0700

    Revert "forgot password"
    This reverts commit 952cde69a36ba3e970770d5051bf8e372e4b8a23.

--- Thriftly/app/src/main/java/com/mastercoding/thriftly/UI/HomeFragment.java
+++ Thriftly/app/src/main/java/com/mastercoding/thriftly/UI/HomeFragment.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,44 +15,24 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.mastercoding.thriftly.Adapter.ProductAdapter;
 import com.mastercoding.thriftly.Authen.SignInActivity;
-import com.mastercoding.thriftly.Models.Product;
+import com.mastercoding.thriftly.MainActivity;
 import com.mastercoding.thriftly.R;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FirebaseAuth mAuth;
-    private RecyclerView recyclerView;
-    private ProductAdapter productAdapter;
-    private List<Product> productList;
-    private FirebaseFirestore db;
 
-    private void bindingView(View view) {
-        recyclerView = view.findViewById(R.id.recyclerView);
-        mAuth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();  // Firestore database
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-
-        bindingView(view);
+        mAuth = FirebaseAuth.getInstance();
         checkUserLoginAndEmailVerification();
 
-        loadProducts();
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
 
-        return view;
     }
 
     private void checkUserLoginAndEmailVerification() {
@@ -64,7 +42,8 @@ public class HomeFragment extends Fragment {
             startActivity(intent);
             getActivity().finish();
         } else {
-            if (!user.isEmailVerified()) {
+            if (user.isEmailVerified()) {
+            } else {
                 mAuth.signOut();
                 Intent intent = new Intent(getActivity(), SignInActivity.class);
                 startActivity(intent);
@@ -74,25 +53,5 @@ public class HomeFragment extends Fragment {
         }
     }
 
-    private void loadProducts() {
-        productList = new ArrayList<>();
-        CollectionReference productsRef = db.collection("Products");
 
-        productsRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        Product product = document.toObject(Product.class);
-                        productList.add(product);
-                    }
-
-                    productAdapter = new ProductAdapter(productList);
-                    recyclerView.setAdapter(productAdapter);
-                } else {
-                    Toast.makeText(getActivity(), "Error getting documents: " + task.getException(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
 }
